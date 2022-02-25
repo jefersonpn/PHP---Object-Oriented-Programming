d6e2ac1c7a89cc20f8e51a825261c2e9371f9817
Jeferson Nascimento <67881060+jefersonpn@users.noreply.github.com>
2022-02-24 20:26:59 -0400

    Structuring code and comments

--- class_instances.php
+++ class_instances.php
@@ -7,13 +7,20 @@ class Student{
 $student1 = new Student;
 $student2 = new Student;
 
-echo get_class($student1) .  "</br>";//checking the name of the class of an object
+// Checking the name of the class of an object.
+echo get_class($student1) .  "</br>";
 
-$class_names = ['Product', 'Student', 'student'];//list of classes in a variable
-foreach($class_names as $class_name){ //loop throw the array using foreach to check if the Object $student1 is instantiated on that class
-    if(is_a($student1, $class_name)) { //if is instantiated prints that
+// List of classes in a variable.
+$class_names = ['Product', 'Student', 'student'];
+// Loop throw the array using foreach to check if the Object $student1 is instantiated on that class.
+foreach($class_names as $class_name):
+
+    if(is_a($student1, $class_name)): // If is instantiated prints that.
+
         echo "student1 is a {$class_name}.</br>";
-     } else {//in case is not instantiated print that
+
+        else: // In case is not instantiated print that.
             echo "student1 is not a {$class_name}.</br>";
-        }
-    }
+
+    endif;
+endforeach;
