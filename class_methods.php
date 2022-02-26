496f645911ec3867b3bdf25787b91ba6bf5a09bb
Jeferson Nascimento <67881060+jefersonpn@users.noreply.github.com>
2022-02-26 07:40:42 -0400

    Creating Methods and tests

--- class_methods.php
+++ class_methods.php
@@ -12,6 +12,9 @@ class Student{
         return 'Hello World!';
     }
 
+    function full_name() {
+        return $this->first_name . " " . $this->last_name;
+    }
 }
 
 // Intantiating a Object.
@@ -25,18 +28,27 @@ $student2->first_name = 'Ethel'; // Filling up the properties.
 $student2->last_name = 'Mertz';
 
 // Printing the first name e last name of both objects.
+echo "<b>Printing the full name using the vars from the object:</b><br />";
 echo $student1->first_name . " " . $student1->last_name . "<br />";
-echo $student2->first_name . " " . $student2->last_name . "<br />";
+echo $student2->first_name . " " . $student2->last_name . "<br /><br />";
+
+// Printing the first name e last name of both objects.
+echo "<b>Printing the full name using the METHOD of the object::</b><br />";
+echo $student1->full_name() . "<br />";
+echo $student2->full_name() . "<br /><br />";
 
 // Calling Methods
+echo "<b>Using METHOD to display msg:</b><br />";
 echo $student1->say_hello() . "<br />";
-echo $student2->say_hello() . "<br />";
+echo $student2->say_hello() . "<br /><br />";
 
 // Checking the defined VARS of the CLASS Student.
+echo "<b>What METHODS are in side the object:</b><br />";
 $class_methods = get_class_methods('Student');
-echo "Class methods: " . implode(', ', $class_methods) . "<br />";
+echo "Class methods: " . implode(', ', $class_methods) . "<br /><br />";
 
 // METHODS_EXISTS() -> This function check if the METHOD are defined in the Object.
+echo "<b>Does the METHODS exists in side the object class?</b><br />";
 if(method_exists('Student', 'say_hello')):
     echo "Method say_hello() exists in Student class.</ br>";
 else:
